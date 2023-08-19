9c379d89490fbdc9038df7d3eb51e4502038e1bc
Gastón Alvarez <gaston_a96@hotmail.com>
2023-08-19 02:55:53 -0300

    creating  dataset

---
+++ mock-data.js
@@ -0,0 +1,946 @@
+export default elements = [
+	{
+		atomicNumber: 1,
+		symbol: "H",
+		name: {
+			en: "Hydrogen",
+			es: "Hidrogeno",
+		},
+	},
+	{
+		atomicNumber: 2,
+		symbol: "He",
+		name: {
+			en: "Helium",
+			es: "Helio",
+		},
+	},
+	{
+		atomicNumber: 3,
+		symbol: "Li",
+		name: {
+			en: "Lithium",
+			es: "Litio",
+		},
+	},
+	{
+		atomicNumber: 4,
+		symbol: "Be",
+		name: {
+			en: "Beryllium",
+			es: "Berilio",
+		},
+	},
+	{
+		atomicNumber: 5,
+		symbol: "B",
+		name: {
+			en: "Boron",
+			es: "Boro",
+		},
+	},
+	{
+		atomicNumber: 6,
+		symbol: "C",
+		name: {
+			en: "Carbon",
+			es: "Carbono",
+		},
+	},
+	{
+		atomicNumber: 7,
+		symbol: "N",
+		name: {
+			en: "Nitrogen",
+			es: "Nitrogeno",
+		},
+	},
+	{
+		atomicNumber: 8,
+		symbol: "O",
+		name: {
+			en: "Oxygen",
+			es: "Oxigeno",
+		},
+	},
+	{
+		atomicNumber: 9,
+		symbol: "F",
+		name: {
+			en: "Fluorine",
+			es: "Fluor",
+		},
+	},
+	{
+		atomicNumber: 10,
+		symbol: "Ne",
+		name: {
+			en: "Neon",
+			es: "Neon",
+		},
+	},
+	{
+		atomicNumber: 11,
+		symbol: "Na",
+		name: {
+			en: "Sodium",
+			es: "Sodio",
+		},
+	},
+	{
+		atomicNumber: 12,
+		symbol: "Mg",
+		name: {
+			en: "Magnesium",
+			es: "Magnesio",
+		},
+	},
+	{
+		atomicNumber: 13,
+		symbol: "Al",
+		name: {
+			en: "Aluminum",
+			es: "Aluminio",
+		},
+	},
+	{
+		atomicNumber: 14,
+		symbol: "Si",
+		name: {
+			en: "Silicon",
+			es: "Silicio",
+		},
+	},
+	{
+		atomicNumber: 15,
+		symbol: "P",
+		name: {
+			en: "Phosphorus",
+			es: "Fosforo",
+		},
+	},
+	{
+		atomicNumber: 16,
+		symbol: "S",
+		name: {
+			en: "Sulfur",
+			es: "Azufre",
+		},
+	},
+	{
+		atomicNumber: 17,
+		symbol: "Cl",
+		name: {
+			en: "Chlorine",
+			es: "Cloro",
+		},
+	},
+	{
+		atomicNumber: 18,
+		symbol: "Ar",
+		name: {
+			en: "Argon",
+			es: "Argon",
+		},
+	},
+	{
+		atomicNumber: 19,
+		symbol: "K",
+		name: {
+			en: "Potassium",
+			es: "Potasio",
+		},
+	},
+	{
+		atomicNumber: 20,
+		symbol: "Ca",
+		name: {
+			en: "Calcium",
+			es: "Calcio",
+		},
+	},
+	{
+		atomicNumber: 21,
+		symbol: "Sc",
+		name: {
+			en: "Scandium",
+			es: "Escandio",
+		},
+	},
+	{
+		atomicNumber: 22,
+		symbol: "Ti",
+		name: {
+			en: "Titanium",
+			es: "Titanio",
+		},
+	},
+	{
+		atomicNumber: 23,
+		symbol: "V",
+		name: {
+			en: "Vanadium",
+			es: "Vanadio",
+		},
+	},
+	{
+		atomicNumber: 24,
+		symbol: "Cr",
+		name: {
+			en: "Chromium",
+			es: "Cromo",
+		},
+	},
+	{
+		atomicNumber: 25,
+		symbol: "Mn",
+		name: {
+			en: "Manganese",
+			es: "Manganeso",
+		},
+	},
+	{
+		atomicNumber: 26,
+		symbol: "Fe",
+		name: {
+			en: "Iron",
+			es: "Hierro",
+		},
+	},
+	{
+		atomicNumber: 27,
+		symbol: "Co",
+		name: {
+			en: "Cobalt",
+			es: "Cobalto",
+		},
+	},
+	{
+		atomicNumber: 28,
+		symbol: "Ni",
+		name: {
+			en: "Nickel",
+			es: "Niquel",
+		},
+	},
+	{
+		atomicNumber: 29,
+		symbol: "Cu",
+		name: {
+			en: "Copper",
+			es: "Cobre",
+		},
+	},
+	{
+		atomicNumber: 30,
+		symbol: "Zn",
+		name: {
+			en: "Zinc",
+			es: "Zinc",
+		},
+	},
+	{
+		atomicNumber: 31,
+		symbol: "Ga",
+		name: {
+			en: "Gallium",
+			es: "Galio",
+		},
+	},
+	{
+		atomicNumber: 32,
+		symbol: "Ge",
+		name: {
+			en: "Germanium",
+			es: "Germanio",
+		},
+	},
+	{
+		atomicNumber: 33,
+		symbol: "As",
+		name: {
+			en: "Arsenic",
+			es: "Arsenico",
+		},
+	},
+	{
+		atomicNumber: 34,
+		symbol: "Se",
+		name: {
+			en: "Selenium",
+			es: "Selenio",
+		},
+	},
+	{
+		atomicNumber: 35,
+		symbol: "Br",
+		name: {
+			en: "Bromine",
+			es: "Bromo",
+		},
+	},
+	{
+		atomicNumber: 36,
+		symbol: "Kr",
+		name: {
+			en: "Krypton",
+			es: "Kripton",
+		},
+	},
+	{
+		atomicNumber: 37,
+		symbol: "Rb",
+		name: {
+			en: "Rubidium",
+			es: "Rubidio",
+		},
+	},
+	{
+		atomicNumber: 38,
+		symbol: "Sr",
+		name: {
+			en: "Strontium",
+			es: "Estroncio",
+		},
+	},
+	{
+		atomicNumber: 39,
+		symbol: "Y",
+		name: {
+			en: "Yttrium",
+			es: "Itrio",
+		},
+	},
+	{
+		atomicNumber: 40,
+		symbol: "Zr",
+		name: {
+			en: "Zirconium",
+			es: "Circonio",
+		},
+	},
+	{
+		atomicNumber: 41,
+		symbol: "Nb",
+		name: {
+			en: "Niobium",
+			es: "Niobio",
+		},
+	},
+	{
+		atomicNumber: 42,
+		symbol: "Mo",
+		name: {
+			en: "Molybdenum",
+			es: "Molibdeno",
+		},
+	},
+	{
+		atomicNumber: 43,
+		symbol: "Tc",
+		name: {
+			en: "Technetium",
+			es: "Tecnecio",
+		},
+	},
+	{
+		atomicNumber: 44,
+		symbol: "Ru",
+		name: {
+			en: "Ruthenium",
+			es: "Rutenio",
+		},
+	},
+	{
+		atomicNumber: 45,
+		symbol: "Rh",
+		name: {
+			en: "Rhodium",
+			es: "Rodio",
+		},
+	},
+	{
+		atomicNumber: 46,
+		symbol: "Pd",
+		name: {
+			en: "Palladium",
+			es: "Paladio",
+		},
+	},
+	{
+		atomicNumber: 47,
+		symbol: "Ag",
+		name: {
+			en: "Silver",
+			es: "Plata",
+		},
+	},
+	{
+		atomicNumber: 48,
+		symbol: "Cd",
+		name: {
+			en: "Cadmium",
+			es: "Cadmio",
+		},
+	},
+	{
+		atomicNumber: 49,
+		symbol: "In",
+		name: {
+			en: "Indium",
+			es: "Indio",
+		},
+	},
+	{
+		atomicNumber: 50,
+		symbol: "Sn",
+		name: {
+			en: "Tin",
+			es: "Estano",
+		},
+	},
+	{
+		atomicNumber: 51,
+		symbol: "Sb",
+		name: {
+			en: "Antimony",
+			es: "Antimonio",
+		},
+	},
+	{
+		atomicNumber: 52,
+		symbol: "Te",
+		name: {
+			en: "Tellurium",
+			es: "Teluro",
+		},
+	},
+	{
+		atomicNumber: 53,
+		symbol: "I",
+		name: {
+			en: "Iodine",
+			es: "Yodo",
+		},
+	},
+	{
+		atomicNumber: 54,
+		symbol: "Xe",
+		name: {
+			en: "Xenon",
+			es: "Xenon",
+		},
+	},
+	{
+		atomicNumber: 55,
+		symbol: "Cs",
+		name: {
+			en: "Cesium",
+			es: "Cesio",
+		},
+	},
+	{
+		atomicNumber: 56,
+		symbol: "Ba",
+		name: {
+			en: "Barium",
+			es: "Bario",
+		},
+	},
+	{
+		atomicNumber: 57,
+		symbol: "La",
+		name: {
+			en: "Lanthanum",
+			es: "Lantano",
+		},
+	},
+	{
+		atomicNumber: 58,
+		symbol: "Ce",
+		name: {
+			en: "Cerium",
+			es: "Cerio",
+		},
+	},
+	{
+		atomicNumber: 59,
+		symbol: "Pr",
+		name: {
+			en: "Praseodymium",
+			es: "Praseodimio",
+		},
+	},
+	{
+		atomicNumber: 60,
+		symbol: "Nd",
+		name: {
+			en: "Neodymium",
+			es: "Neodimio",
+		},
+	},
+	{
+		atomicNumber: 61,
+		symbol: "Pm",
+		name: {
+			en: "Promethium",
+			es: "Prometio",
+		},
+	},
+	{
+		atomicNumber: 62,
+		symbol: "Sm",
+		name: {
+			en: "Samarium",
+			es: "Samario",
+		},
+	},
+	{
+		atomicNumber: 63,
+		symbol: "Eu",
+		name: {
+			en: "Europium",
+			es: "Europio",
+		},
+	},
+	{
+		atomicNumber: 64,
+		symbol: "Gd",
+		name: {
+			en: "Gadolinium",
+			es: "Gadolin",
+		},
+	},
+	{
+		atomicNumber: 65,
+		symbol: "Tb",
+		name: {
+			en: "Terbium",
+			es: "Terbio",
+		},
+	},
+	{
+		atomicNumber: 66,
+		symbol: "Dy",
+		name: {
+			en: "Dysprosium",
+			es: "Disprosio",
+		},
+	},
+	{
+		atomicNumber: 67,
+		symbol: "Ho",
+		name: {
+			en: "Holmium",
+			es: "Holmio",
+		},
+	},
+	{
+		atomicNumber: 68,
+		symbol: "Er",
+		name: {
+			en: "Erbium",
+			es: "Erbio",
+		},
+	},
+	{
+		atomicNumber: 69,
+		symbol: "Tm",
+		name: {
+			en: "Thulium",
+			es: "Tulio",
+		},
+	},
+	{
+		atomicNumber: 70,
+		symbol: "Yb",
+		name: {
+			en: "Ytterbium",
+			es: "Iterbio",
+		},
+	},
+	{
+		atomicNumber: 71,
+		symbol: "Lu",
+		name: {
+			en: "Lutetium",
+			es: "Lutecio",
+		},
+	},
+	{
+		atomicNumber: 72,
+		symbol: "Hf",
+		name: {
+			en: "Hafnium",
+			es: "Hafnio",
+		},
+	},
+	{
+		atomicNumber: 73,
+		symbol: "Ta",
+		name: {
+			en: "Tantalum",
+			es: "Tantalio",
+		},
+	},
+	{
+		atomicNumber: 74,
+		symbol: "W",
+		name: {
+			en: "Tungsten",
+			es: "Wolframio",
+		},
+	},
+	{
+		atomicNumber: 75,
+		symbol: "Re",
+		name: {
+			en: "Rhenium",
+			es: "Renio",
+		},
+	},
+	{
+		atomicNumber: 76,
+		symbol: "Os",
+		name: {
+			en: "Osmium",
+			es: "Osmio",
+		},
+	},
+	{
+		atomicNumber: 77,
+		symbol: "Ir",
+		name: {
+			en: "Iridium",
+			es: "Iridio",
+		},
+	},
+	{
+		atomicNumber: 78,
+		symbol: "Pt",
+		name: {
+			en: "Platinum",
+			es: "Platino",
+		},
+	},
+	{
+		atomicNumber: 79,
+		symbol: "Au",
+		name: {
+			en: "Gold",
+			es: "Oro",
+		},
+	},
+	{
+		atomicNumber: 80,
+		symbol: "Hg",
+		name: {
+			en: "Mercury",
+			es: "Mercurio",
+		},
+	},
+	{
+		atomicNumber: 81,
+		symbol: "Tl",
+		name: {
+			en: "Thallium",
+			es: "Talio",
+		},
+	},
+	{
+		atomicNumber: 82,
+		symbol: "Pb",
+		name: {
+			en: "Lead",
+			es: "Plomo",
+		},
+	},
+	{
+		atomicNumber: 83,
+		symbol: "Bi",
+		name: {
+			en: "Bismuth",
+			es: "Bismuto",
+		},
+	},
+	{
+		atomicNumber: 84,
+		symbol: "Po",
+		name: {
+			en: "Polonium",
+			es: "Polonio",
+		},
+	},
+	{
+		atomicNumber: 85,
+		symbol: "At",
+		name: {
+			en: "Astatine",
+			es: "Astato",
+		},
+	},
+	{
+		atomicNumber: 86,
+		symbol: "Rn",
+		name: {
+			en: "Radon",
+			es: "Radon",
+		},
+	},
+	{
+		atomicNumber: 87,
+		symbol: "Fr",
+		name: {
+			en: "Francium",
+			es: "Francio",
+		},
+	},
+	{
+		atomicNumber: 88,
+		symbol: "Ra",
+		name: {
+			en: "Radium",
+			es: "Radio",
+		},
+	},
+	{
+		atomicNumber: 89,
+		symbol: "Ac",
+		name: {
+			en: "Actinium",
+			es: "Actinio",
+		},
+	},
+	{
+		atomicNumber: 90,
+		symbol: "Th",
+		name: {
+			en: "Thorium",
+			es: "Torio",
+		},
+	},
+	{
+		atomicNumber: 91,
+		symbol: "Pa",
+		name: {
+			en: "Protactinium",
+			es: "Protactinio",
+		},
+	},
+	{
+		atomicNumber: 92,
+		symbol: "U",
+		name: {
+			en: "Uranium",
+			es: "Uranio",
+		},
+	},
+	{
+		atomicNumber: 93,
+		symbol: "Np",
+		name: {
+			en: "Neptunium",
+			es: "Neptunio",
+		},
+	},
+	{
+		atomicNumber: 94,
+		symbol: "Pu",
+		name: {
+			en: "Plutonium",
+			es: "Plutonio",
+		},
+	},
+	{
+		atomicNumber: 95,
+		symbol: "Am",
+		name: {
+			en: "Americium",
+			es: "Americio",
+		},
+	},
+	{
+		atomicNumber: 96,
+		symbol: "Cm",
+		name: {
+			en: "Curium",
+			es: "Curio",
+		},
+	},
+	{
+		atomicNumber: 97,
+		symbol: "Bk",
+		name: {
+			en: "Berkelium",
+			es: "Berkelio",
+		},
+	},
+	{
+		atomicNumber: 98,
+		symbol: "Cf",
+		name: {
+			en: "Californium",
+			es: "Californio",
+		},
+	},
+	{
+		atomicNumber: 99,
+		symbol: "Es",
+		name: {
+			en: "Einsteinium",
+			es: "Einstenio",
+		},
+	},
+	{
+		atomicNumber: 100,
+		symbol: "Fm",
+		name: {
+			en: "Fermium",
+			es: "Fermio",
+		},
+	},
+	{
+		atomicNumber: 101,
+		symbol: "Md",
+		name: {
+			en: "Mendelevium",
+			es: "Mendelevio",
+		},
+	},
+	{
+		atomicNumber: 102,
+		symbol: "No",
+		name: {
+			en: "Nobelium",
+			es: "Nobelio",
+		},
+	},
+	{
+		atomicNumber: 103,
+		symbol: "Lr",
+		name: {
+			en: "Lawrencium",
+			es: "Laurencio",
+		},
+	},
+	{
+		atomicNumber: 104,
+		symbol: "Rf",
+		name: {
+			en: "Rutherfordium",
+			es: "Rutherfordio",
+		},
+	},
+	{
+		atomicNumber: 105,
+		symbol: "Db",
+		name: {
+			en: "Dubnium",
+			es: "Dubnio",
+		},
+	},
+	{
+		atomicNumber: 106,
+		symbol: "Sg",
+		name: {
+			en: "Seaborgium",
+			es: "Seaborgio",
+		},
+	},
+	{
+		atomicNumber: 107,
+		symbol: "Bh",
+		name: {
+			en: "Bohrium",
+			es: "Bohrio",
+		},
+	},
+	{
+		atomicNumber: 108,
+		symbol: "Hs",
+		name: {
+			en: "Hassium",
+			es: "Hassio",
+		},
+	},
+	{
+		atomicNumber: 109,
+		symbol: "Mt",
+		name: {
+			en: "Meitnerium",
+			es: "Meitnerio",
+		},
+	},
+	{
+		atomicNumber: 110,
+		symbol: "Ds",
+		name: {
+			en: "Darmstadtium",
+			es: "Darmstadtio",
+		},
+	},
+	{
+		atomicNumber: 111,
+		symbol: "Rg",
+		name: {
+			en: "Roentgenium",
+			es: "Roentgenio",
+		},
+	},
+	{
+		atomicNumber: 112,
+		symbol: "Cn",
+		name: {
+			en: "Copernicium",
+			es: "Copernicio",
+		},
+	},
+	{
+		atomicNumber: 113,
+		symbol: "Nh",
+		name: {
+			en: "Nihonium",
+			es: "Nihonio",
+		},
+	},
+	{
+		atomicNumber: 114,
+		symbol: "Fl",
+		name: {
+			en: "Flerovium",
+			es: "Flerovio",
+		},
+	},
+	{
+		atomicNumber: 115,
+		symbol: "Mc",
+		name: {
+			en: "Moscovium",
+			es: "Moscovio",
+		},
+	},
+	{
+		atomicNumber: 116,
+		symbol: "Lv",
+		name: {
+			en: "Livermorium",
+			es: "Livermorio",
+		},
+	},
+	{
+		atomicNumber: 117,
+		symbol: "Ts",
+		name: {
+			en: "Tennessine",
+			es: "Tenesino",
+		},
+	},
+	{
+		atomicNumber: 118,
+		symbol: "Og",
+		name: {
+			en: "Oganesson",
+			es: "Oganeson",
+		},
+	},
+];
